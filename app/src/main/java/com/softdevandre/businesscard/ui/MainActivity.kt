71c56edd0167fbbf6aac25ecef16b04105e139c1
softdevandre <softdev.andre@gmail.com>
2022-08-15 13:35:59 -0300

    implemented share card image

--- app/src/main/java/com/softdevandre/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/softdevandre/businesscard/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.softdevandre.businesscard.App
 import com.softdevandre.businesscard.databinding.ActivityMainBinding
+import com.softdevandre.businesscard.util.Image
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,6 +29,9 @@ class MainActivity : AppCompatActivity() {
         binding.fabAdd.setOnClickListener {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
+        }
+        adapter.listenerShare = { card ->
+            Image.share(this@MainActivity, card)
         }
     }
 
